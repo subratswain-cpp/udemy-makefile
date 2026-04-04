32104cf0dd14f6314ecca6d24ba0c95c995d3f74
subratswain-cpp <cppwithsubrat@gmail.com>
2026-04-04 10:23:15 +0530

    [UDATE]:Mod2-Test Using CMake

--- makefile_demo_cpp/src/module_2/test_mod2.cpp
+++ makefile_demo_cpp/src/module_2/test_mod2.cpp
@@ -2,7 +2,8 @@
 #include "mod2.h"
 
 int main() {
-    std::cout << "XXXXXI am testing so\n";   
+    // std::cout << "XXXXXI am testing so\n"; // It was in Makefile recording.
+    std::cout << "I am testing .so using CMake\n"; // Updated during CMake demo
     fun2();
     return EXIT_SUCCESS;
 }
